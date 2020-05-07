b27cae79e473d38d381c18da12abe4e5c724b6d5
Ben Schmidt <bens@polyswarm.io>
2020-05-07 00:52:01 -0400

    fix logging

--- src/polyd_to_s3/logging.py
+++ src/polyd_to_s3/logging.py
@@ -3,11 +3,17 @@ import logging
 level = logging.INFO
 
 
+logger = None
+
+
 def get_logger():
-    logger = logging.getLogger('polyd-to-s3')
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(level)
+    global logger
+    if not logger:
+        logger = logging.getLogger('polyd-to-s3')
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(level)
     return logger
+
